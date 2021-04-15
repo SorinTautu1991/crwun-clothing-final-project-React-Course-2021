6bd93d422c7af32e41b4db12a2943a2ec320791b
SorinTautu1991 <sorin.tautu@gmail.com>
2021-04-15 11:01:37 +0300

    Updating our app with the shop page etc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 
 const HatsPage = () => (
@@ -14,7 +15,7 @@ function App() {
     <div>
       <Switch>
         <Route exact={true} path="/" component={HomePage} />
-        <Route exact={true} path="/hats" component={HatsPage} />
+        <Route exact={true} path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
