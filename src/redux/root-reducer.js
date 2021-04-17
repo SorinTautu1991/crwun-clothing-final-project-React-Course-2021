a790288cd71a636396a32e657880b0eac7126b73
SorinTautu1991 <sorin.tautu@gmail.com>
2021-04-17 11:41:30 +0300

    Added cart icon and cart dropdown to our app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from '../redux/user/user.reducer';
+import cartReducer from '../redux/cart/cart.reducer';
 
 export default combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
