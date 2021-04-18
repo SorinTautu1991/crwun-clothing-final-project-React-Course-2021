7788688792a4873560c254da035e5a59dc6596f8
SorinTautu1991 <sorin.tautu@gmail.com>
2021-04-18 16:41:59 +0300

    Redux flow for certain directories

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from '../redux/user/user.reducer';
 import cartReducer from '../redux/cart/cart.reducer';
+import directoryReducer from '../redux/directory/directory.reducer';
+import shopReducer from '../redux/shop/shop.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+export default persistReducer(persistConfig, rootReducer);
